fb1dbcd5cd0b9c53ac45a196a651adc7682912d9
JeonJun202 <jjg010202@gmail.com>
2024-12-31 10:31:03 +0900

    미디어 쿼리 추가

--- components/inputDropdown/InputDropdownGenre.jsx
+++ components/inputDropdown/InputDropdownGenre.jsx
@@ -2,14 +2,17 @@ import InputDropdownLayout from './InputDropdownLayout';
 import { useState } from 'react';
 
 export default function InputDropdownGenre() {
+  // 옵션들. 필요시 변경 혹은 추가
   const options = [
     { label: '여행', value: 'travel' },
     { label: '풍경', value: 'landscape' },
     { label: '인물', value: 'person' },
     { label: '사물', value: 'objects' },
   ];
+  // 선택된 드롭다운 초기값은은 따로 
   const [genre, setGenre] = useState({ label: '장르를 선택해주세요', value: '' });
-  //   const genreValue = genre.value;
+  //나중에 api에 값 필요하면 쓸거
+  //   const genreValue = genre.value; 
   const handleSelectOption = (selectedOption) => setGenre(selectedOption);
 
   return (
