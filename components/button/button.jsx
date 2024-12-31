48b5d33c5ed4a4f65bb505ca41f4f52762a88ada
Kimhyunmook <gusanr4200@naver.com>
2024-12-31 17:45:28 +0900

    button 수정

--- components/button/button.jsx
+++ components/button/button.jsx
@@ -1,49 +1,44 @@
 import { useEffect, useState } from 'react';
 import styles from '@/styles/button.module.css';
 
-const Size = [
-  {
-    width: 440,
-    height: 80,
-  },
-  {
-    width: 440,
-    height: 55,
-  },
-  {
+const Size = {
+  xxxl: {
     width: 520,
     height: 60,
   },
-  {
+  xxl: {
+    width: 440,
+    height: 80,
+  },
+  xl: {
+    width: 440,
+    height: 55,
+  },
+
+  l: {
     width: 345,
     height: 75,
   },
-  {
+  m: {
     width: 345,
     height: 55,
   },
-  {
+  s: {
     width: 345,
     height: 55,
   },
-  {
+  xs: {
     width: 342,
     height: 75,
   },
-  {
+  xxs: {
     width: 150,
     height: 40,
     fontSize: '12px',
     fontWeight: 700,
   },
-];
-const texts = {
-  buy: '포토카드 구매하기',
-  trade: '포토카드 교환하기',
-  sellOff: '판매 내리기',
-  allow: '승인',
-  reject: '거절',
 };
+
 const init = {
   dispaly: 'flex',
   alignItems: 'center',
@@ -54,19 +49,18 @@ const init = {
  * @param {ButtonProps} props
  * @param type primary/primary_disabled/secondary
  * @param onClick onClick event
- * @param text buy: '포토카드 구매하기'/ trade: '포토카드 교환하기'/ sellOff: '판매 내리기'/ allow: '승인' / reject: '거절 / free: children 으로 값 지정
  * @param size Int///
- * 0: {width: 440,height: 80} ///
-   1: {width: 440,height: 55} ///
-   2: {width: 520,height: 60} ///
-   3: {width: 345,height: 75} ///
-   4: {width: 345,height: 55} ///
-   5: {width: 345,height: 55} ///
-   6: {width: 342,height: 75} ///
-   7: {width: 150,height: 40} ///
+ * xxxl: {width: 520,height: 60} ///
+    xxl: {width: 440,height: 80} ///
+     xl: {width: 440,height: 55} ///
+      l: {width: 345,height: 75} ///
+      m: {width: 345,height: 55} ///
+      s: {width: 345,height: 55} ///
+     xs: {width: 342,height: 75} ///
+    xxs: {width: 150,height: 40} ///
  * @returns
  */
-export default function Button({ type = 'primary', size = 0, text = 'free', onClick, children }) {
+export default function Button({ type = 'primary', size = '', onClick, children }) {
   const [btnStyle, setBtnStyle] = useState({
     ...init,
     ...Size[size],
@@ -105,7 +99,7 @@ export default function Button({ type = 'primary', size = 0, text = 'free', onCl
   if (isLoading) return null;
   return (
     <button className={styles.Button} onClick={onClick} style={btnStyle}>
-      {text === 'free' ? children : texts[text]}
+      {children}
     </button>
   );
 }
