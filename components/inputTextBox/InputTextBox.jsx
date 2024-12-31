5b32d2ec1433689518e218ff6711745998759af6
Jinhan <wlsgks333@naver.com>
2024-12-31 16:41:41 +0900

    feat: Input File Upload 컴포넌트 구현

---
+++ components/inputTextBox/InputTextBox.jsx
@@ -0,0 +1,10 @@
+import styles from '@/styles/components/InputTextBox/InputTextBox.module.css';
+
+export default function InputTextBox() {
+  return (
+    <>
+      <h1>포토카드 설명</h1>
+      <input type="file" />
+    </>
+  );
+}
