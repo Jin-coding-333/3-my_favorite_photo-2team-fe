2a436cbf080fa5b1f138f44faf81b2958898aa24
taehoone <ghost111789@gmail.comm>
2025-01-03 13:53:38 +0900

    input공통컴포넌트: 기능구현, 폴더명 대문자 변경

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,28 +1,6 @@
-import Input from '@/components/input/inputCommon'; // input공통컴포넌트 import
-import { useState } from 'react';
-
 export default function Login() {
-  // const [Email, setEmail] = useState();
-  // const [password, setPassword] = useState();
-  // const [photoName, setPhotoName] = useState();
-  // const [price, setPrice] = useState();
-  // const [quantity, setQuantity] = useState();
-  // const [nickName, setNickName] = useState();
-  // const [passwordChk, setPasswordChk] = useState();
-
   return (
     <>
-      {/* <Input type="email" value={Email} onChange={(e) => setEmail(e.target.value)} />
-      <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <Input type="photoName" value={photoName} onChange={(e) => setPhotoName(e.target.value)} />
-      <Input type="price" value={price} onChange={(e) => setPrice(e.target.value)} />
-      <Input type="quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-      <Input type="nickName" value={nickName} onChange={(e) => setNickName(e.target.value)} />
-      <Input
-        type="passwordChk"
-        value={passwordChk}
-        onChange={(e) => setPasswordChk(e.target.value)} */}
-      />
       <h1>Login</h1>
     </>
   );
