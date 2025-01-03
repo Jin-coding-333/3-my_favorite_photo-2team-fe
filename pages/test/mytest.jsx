7d475549a7fb5631bfa78e5cf63c6928bbeaa7df
kimhyunmook_mac <gusanr4200@naver.com>
2025-01-03 12:43:13 +0900

    폰트 추가 및 간단 수정

--- pages/test/mytest.jsx
+++ pages/test/mytest.jsx
@@ -4,9 +4,10 @@ import Card from '@/components/_card/Card';
 
 export default function MyTest() {
   return (
-    <>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <BottomSheet />
       <Card>안녕하십니까</Card>
       <Card type={'sell'}>안녕하십니까</Card>
-    </>
+    </div>
   );
 }
