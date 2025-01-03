1f9aacf90cc7c7ea6c3dbee6eb743857bf7c8871
kimhyunmook_mac <gusanr4200@naver.com>
2025-01-03 16:42:23 +0900

    마무리

--- components/_card/Card.jsx
+++ components/_card/Card.jsx
@@ -3,6 +3,7 @@ import Button from '../button/Button';
 import { useEffect, useState } from 'react';
 import { useFirstUpperCase } from '@/lib/hooks/convenience';
 import ImgHover from '../ImgHover';
+import { BtnPlace, Title, Content, CountBox } from './Card.Component';
 
 /**
  *
@@ -27,67 +28,26 @@ export default function Card({
         setElement(<SellerBottom />);
         break;
       default:
-        null;
+        setElement(<MyBottom type={type}>{children}</MyBottom>);
         break;
     }
-  }, [type]);
+  }, [type, children]);
+
   return (
     <div className={`${styles.Card} ${styles[_type]}`}>
       <Title grade={grade} genre={genre} userNickName={userNickName} />
-      <Content price={0} count={0}>
-        {children}
-      </Content>
-      {element}
-    </div>
-  );
-}
-
-function Title({ grade = 'COMMON', genre = 'unknwon', userNickName }) {
-  return (
-    <div className={styles.Title}>
-      <div className={styles.Top}>
-        <p className={styles.Grade}>{grade}</p>
-        <div className={styles.TitleLine}></div>
-        <p className={styles.Genre}>{genre}</p>
-      </div>
-      {userNickName && <div className={styles.UserNickName}>{userNickName}</div>}
-    </div>
-  );
-}
-
-function Content({ children = '', price = 0, count = 0, totalCount = 0, onlyText = false }) {
-  return (
-    <div className={styles.Content}>
-      <div className={styles.Text}>{children}</div>
-      {onlyText ? null : (
-        <ul className={styles.Bottom}>
-          <li className={styles.Price}>
-            <p className={styles.Left}>가격</p>
-            <div className={styles.Right}>{price} P</div>
-          </li>
-          <li>
-            <p className={styles.Left}>잔여</p>
-            <div className={styles.Right}>
-              <span className={styles.Current}>{count}</span>
-              <span className={`${styles.Gray} ${styles.Slash}`}>/</span>
-              <span className={styles.Gray}>{totalCount}</span>
-            </div>
-          </li>
-        </ul>
+      {!type.includes('my') && (
+        <Content price={0} count={0} totalCount={0}>
+          {children}
+        </Content>
       )}
+      {element}
     </div>
   );
 }
 
 function BuyBottom({ point = 0, totalCount = 0 }) {
   const [count, setCount] = useState(0);
-  function increase() {
-    if (count >= totalCount) return alert('최대 구매 가능 수량입니다.');
-    setCount(count + 1);
-  }
-  function decrease() {
-    setCount(count - 1);
-  }
   const buyBtn = [
     {
       type: 'primary',
@@ -103,15 +63,7 @@ function BuyBottom({ point = 0, totalCount = 0 }) {
       <ul className={styles.BuyBottom}>
         <li>
           <p className={styles.Left}>구매 수량</p>
-          <div className={styles.CountBox}>
-            <button className={styles.minus} onClick={decrease}>
-              -
-            </button>
-            <input type="number" value={count} />
-            <button className={styles.plus} onClick={increase}>
-              +
-            </button>
-          </div>
+          <CountBox count={count} setCount={setCount} totalCount={totalCount} />
         </li>
         <li>
           <p className={styles.Left}>총 가격</p>
@@ -125,7 +77,11 @@ function BuyBottom({ point = 0, totalCount = 0 }) {
   );
 }
 
-function SellerBottom({ children = 'I want to exchange this card' }) {
+function SellerBottom({
+  children = 'I want to exchange this card',
+  grade = 'COMMON',
+  genre = 'unknwon',
+}) {
   const sellBtn = [
     {
       type: 'primary',
@@ -158,7 +114,7 @@ function SellerBottom({ children = 'I want to exchange this card' }) {
         />
         교환 희망 정보
       </p>
-      <Title />
+      <Title grade={grade} genre={genre} />
       <Content onlyText={true}>{children}</Content>
       {sellBtn.map((v, i) => {
         return <Button key={i} {...v} />;
@@ -167,12 +123,33 @@ function SellerBottom({ children = 'I want to exchange this card' }) {
   );
 }
 
-function BtnPlace({ btn = [] }) {
+function MyBottom({ type = 'mySell', children = '', point = 0, totalCount = 0 }) {
+  const myBtn = [
+    {
+      type: 'primary',
+      size: 'xxl',
+      children: type === 'mySell' ? '포토카드 판매하기' : '포토카드 구매하기',
+      onClick: () => {
+        alert('포토카드 판매하기');
+      },
+    },
+  ];
   return (
-    <div className={styles.BtnPlace}>
-      {btn.map((v, i) => (
-        <Button key={i} {...v} />
-      ))}
+    <div className={styles.MyBottom}>
+      <div className={styles.Content}>
+        <p className={styles.Text}>{children}</p>
+      </div>
+      <ul>
+        <li>
+          <p className={styles.Left}>가격</p>
+          <p className={styles.Right}>{point} P</p>
+        </li>
+        <li>
+          <p className={styles.Left}>보유량</p>
+          <p className={styles.Right}>{totalCount}</p>
+        </li>
+      </ul>
+      <BtnPlace btn={myBtn} />
     </div>
   );
 }
--- pages/test/mytest.jsx
+++ pages/test/mytest.jsx
@@ -1,13 +1,14 @@
-import BottomSheet from '@/components/bottomSheet/BottomSheet';
-import Button from '@/components/button/Button';
 import Card from '@/components/_card/Card';
+import CardDetail from '@/components/_card/CardDetail';
 
 export default function MyTest() {
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      <BottomSheet />
       <Card>안녕하십니까</Card>
       <Card type={'sell'}>안녕하십니까</Card>
+      <CardDetail />
+      <Card type={'mySell'}></Card>
+      <Card type={'myBuy'}>응ㅇ </Card>
     </div>
   );
 }
