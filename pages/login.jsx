e6b570d02ac8e5ecf15db898835e12baddf6d6c6
taehoone <ghost111789@gmail.comm>
2025-01-03 13:41:09 +0900

    input 공통컴포넌트: 기능구현

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,18 +1,18 @@
-import Input from '@/components/input/inputCommon';
+import Input from '@/components/input/inputCommon'; // input공통컴포넌트 import
 import { useState } from 'react';
 
 export default function Login() {
-  const [Email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [photoName, setPhotoName] = useState();
-  const [price, setPrice] = useState();
-  const [quantity, setQuantity] = useState();
-  const [nickName, setNickName] = useState();
-  const [passwordChk, setPasswordChk] = useState();
+  // const [Email, setEmail] = useState();
+  // const [password, setPassword] = useState();
+  // const [photoName, setPhotoName] = useState();
+  // const [price, setPrice] = useState();
+  // const [quantity, setQuantity] = useState();
+  // const [nickName, setNickName] = useState();
+  // const [passwordChk, setPasswordChk] = useState();
 
   return (
     <>
-      <Input type="email" value={Email} onChange={(e) => setEmail(e.target.value)} />
+      {/* <Input type="email" value={Email} onChange={(e) => setEmail(e.target.value)} />
       <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       <Input type="photoName" value={photoName} onChange={(e) => setPhotoName(e.target.value)} />
       <Input type="price" value={price} onChange={(e) => setPrice(e.target.value)} />
@@ -21,9 +21,8 @@ export default function Login() {
       <Input
         type="passwordChk"
         value={passwordChk}
-        onChange={(e) => setPasswordChk(e.target.value)}
+        onChange={(e) => setPasswordChk(e.target.value)} */}
       />
-
       <h1>Login</h1>
     </>
   );
