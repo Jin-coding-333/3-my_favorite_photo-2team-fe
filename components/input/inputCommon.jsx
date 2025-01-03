07901d78b57296bea62cf1456c7de6ddea14f4fa
taehoone <ghost111789@gmail.comm>
2025-01-04 01:17:14 +0900

    input공통컴포넌트 기능구현 미디어쿼리

--- components/input/inputCommon.jsx
+++ components/input/inputCommon.jsx
@@ -103,3 +103,36 @@ export default function Input({
     </div>
   );
 }
+
+//사용예시
+// import { useState } from 'react';
+// import Input from '@/components/input/InputCommon';
+
+// export default function Login() {
+//   const [Email, setEmail] = useState();
+//   const [password, setPassword] = useState();
+//   const [photoName, setPhotoName] = useState();
+//   const [price, setPrice] = useState();
+//   const [quantity, setQuantity] = useState();
+//   const [nickName, setNickName] = useState();
+//   const [passwordChk, setPasswordChk] = useState();
+
+//   return (
+//     <>
+//       <Input type="email" value={Email} onChange={(e) => setEmail(e.target.value)} />
+//       <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+//       <Input
+//         type="passwordChk"
+//         value={passwordChk}
+//         onChange={(e) => setPasswordChk(e.target.value)}
+//         passwordToCompare={password} // 비밀번호와 비밀번호 확인 검증코드
+//       />
+//       <Input type="photoName" value={photoName} onChange={(e) => setPhotoName(e.target.value)} />
+//       <Input type="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+//       <Input type="quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+//       <Input type="nickName" value={nickName} onChange={(e) => setNickName(e.target.value)} />
+
+//       <h1>Login</h1>
+//     </>
+//   );
+// }
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,31 +1,7 @@
-import { useState } from 'react';
-import Input from '@/components/input/InputCommon';
-
 export default function Login() {
-  const [Email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [photoName, setPhotoName] = useState();
-  const [price, setPrice] = useState();
-  const [quantity, setQuantity] = useState();
-  const [nickName, setNickName] = useState();
-  const [passwordChk, setPasswordChk] = useState();
-
   return (
     <>
-      <Input type="email" value={Email} onChange={(e) => setEmail(e.target.value)} />
-      <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <Input
-        type="passwordChk"
-        value={passwordChk}
-        onChange={(e) => setPasswordChk(e.target.value)}
-        passwordToCompare={password}
-      />
-      <Input type="photoName" value={photoName} onChange={(e) => setPhotoName(e.target.value)} />
-      <Input type="price" value={price} onChange={(e) => setPrice(e.target.value)} />
-      <Input type="quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-      <Input type="nickName" value={nickName} onChange={(e) => setNickName(e.target.value)} />
-
-      {/* <h1>Login</h1> */}
+      <h1>Login</h1>
     </>
   );
 }
