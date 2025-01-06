2b08626ab97c2fd474b0f41725bd35009dfe747d
heziss <heziss0965@gmail.com>
2025-01-06 17:20:29 +0900

    feat: title 컴포넌트 css작성 및 테스트

--- components/title/Title.jsx
+++ components/title/Title.jsx
@@ -2,18 +2,26 @@ import styles from '@/styles/components/title/Title.module.css'
 import Button from '../button/button'
 
 export default function Title({
-  title, buttonText, onButtonClick, variant = 'default'
+  title, buttonText, onButtonClick, variant = 'default', buttonType="primary",
 }) {
+  //폰트 설정
   const titleClass = variant === 'default' ? styles.defaultTitle : styles.secondaryTitle;
-
+  
   return (
-    <div className={styles.titleContainer}>
+      <div className={styles.titleContainer}>
       <h2 className={titleClass}>{title}</h2>
       {buttonText && (
-        <Button type='primary' size='l' onClick={onButtonClick}>
+        <Button type={buttonType} size='l' onClick={onButtonClick}>
           {buttonText}
         </Button>
       )}
     </div>
+    
   )
 }
+//사용 방법
+{/* <Title
+  title="마켓플레이스"
+  buttonText="내 포토카드 판매하기"
+  onButtonClick={() => alert('판매하기 버튼이 클릭됨')}
+  /> */}
