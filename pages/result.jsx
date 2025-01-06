fcb5d8e07acd780aedc1f629cbabb2b713d8c4f7
Jinhan <wlsgks333@naver.com>
2025-01-06 15:28:11 +0900

    refactor: result 페이지 수정

--- pages/result.jsx
+++ pages/result.jsx
@@ -1,71 +1,117 @@
 import styles from "@/styles/pages/Result.module.css";
 import Button from "@/components/button/Button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Result() {
 
-  const result = {
-    success: '성공',
-    fail: '실패',
-  };
+  const result = 'success'
+  const type = 'exchangeSuggestionRegistration'
 
-  const backPage = {
-    MarketPlace1: {
-      title: '판매 등록 결과 페이지',
-      success: '나의 판매 포토카드에서 확인하기',
-      fail: '마켓플레이스로 돌아가기',
-    },
-    MarketPlace2: {
-      title: '구매 결과 페이지',
-      success: '마이갤러리에서 확인하기',
-      fail: '마켓플레이스로 돌아가기',
-    },
-    MarketPlace3: {
-      title: '교환 제시 결과 페이지',
-      success: '나의 판매 포토카드에서 확인하기',
-      fail: '마켓플레이스로 돌아가기',
-    },
-    MyGallery: {
-      title: '포토카드 생성 결과 페이지',
-      success: '마이갤러리에서 확인하기',
-      fail: '마이갤러리로 돌아가기',
-    },
+  let content = {
+    mainText: '',
+    subText: '',
+    buttonText: ''
+  }
+
+  switch (type) {
+    case 'salesRegistration':
+      content = {
+        message: '판매 등록',
+        buttonText:
+          result === 'success' ?
+            '나의 판매 포토카드에서 확인하기' :
+            '마켓플레이스로 돌아가기',
+        buttonUrl:
+          result === 'success' ?
+            '/my-sale-photocard' :
+            '/',
+      };
+      break;
+    case 'purchaseRegistration':
+      content = {
+        message: '구매',
+        buttonText:
+          result === 'success' ?
+            '마이갤러리에서 확인하기' :
+            '마켓플레이스로 돌아가기',
+        buttonUrl:
+          result === 'success' ?
+            '/my-gallery' :
+            '/',
+      };
+      break;
+    case 'exchangeSuggestionRegistration':
+      content = {
+        message: '교환 제시',
+        buttonText:
+          result === 'success' ?
+            '나의 판매 포토카드에서 확인하기' :
+            '마켓플레이스로 돌아가기',
+        buttonUrl:
+          result === 'success' ?
+            '/my-sale-photocard' :
+            '/',
+      };
+      break;
+    case 'creationPhotocardRegistration':
+      content = {
+        message: '포토카드 생성',
+        buttonText:
+          result === 'success' ?
+            '마이갤러리에서 확인하기' :
+            '마이갤러리로 돌아가기',
+        buttonUrl:
+          result === 'success' ?
+            '/my-gallery' :
+            `/my-gallery/photocard/${'photocardId'}`,
+      };
+      break;
+    case '':
+      alert('type을 부모 컴포넌트에서 입력해 주세요');
+      break;
   }
 
   return (
     <div className={styles.resultContainer}>
       <div className={styles.resultBox}>
         <h1 className={styles.mainText}>
-          판매 등록 <span className={styles.resultText}>{'성공'}</span>
+          {content.message}
+          {result === 'success' ?
+            <span className={styles.resultSuccessText}> 성공</span> :
+            <span className={styles.resultFailureText}> 실패</span>}
         </h1>
         <div className={styles.subTextBox}>
           <p className={styles.subText}>
-            [{'LEGENDARY'}| {'우리집 앞마당'}] {'2'}장 판매 등록에 {'성공'}했습니다!
+            [{'LEGENDARY'}| {'우리집 앞마당'}] {0}장 {content.message}에
+            {result === 'success' ? ' 성공' : ' 실패'}했습니다!
           </p>
         </div>
-        <Button
-          className={styles.buttonComponent}
-          type="secondary" size="xl">
-          나의 판매 포토카드에서 확인하기
-        </Button>
+        <Link href={`${content.buttonUrl}`}>
+          <Button
+            className={styles.buttonComponent}
+            type="secondary" size="xl">
+            {content.buttonText}
+          </Button>
+        </Link>
         {/* x 버튼 */}
         <div className={styles.closeBtn} />
       </div>
       <div
-          className={styles.backPageButton}
-          style={{
-            position: 'relative',
+        className={styles.backPageButton}
+        style={{
+          position: 'relative',
+        }}
+      >
+        <Image
+          src="/icon/type=back.png"
+          alt="backPage button"
+          fill
+          styles={{
+            objectFit: 'cover',
           }}
-        >
-          <Image
-            src="/icon/type=back.png"
-            alt="backPage button"
-            fill
-            styles={{
-              objectFit: 'cover',
-            }}
-          />
-        </div>
+        />
+      </div>
     </div >
   );
 }
