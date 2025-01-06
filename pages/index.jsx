fc6d0670aadba7f6b12b5dfac1e6fe2a3685e42d
Jinhan <wlsgks333@naver.com>
2025-01-06 13:18:07 +0900

    feat: CardCountStatus 컴포넌트 기본적인 디자인 구현

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
+import CardCountStatus from "@/components/title/CardCountStatus";
 
 export default function MarketPlace() {
   return (
     <>
       <h1>Market Place</h1>
+      <CardCountStatus page='mySalePhotocard' />
     </>
   );
 }
