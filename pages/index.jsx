67f30525f0f7bfec5395960b52dc0f525ec901f3
Jinhan <wlsgks333@naver.com>
2025-01-06 13:37:25 +0900

    chore: index 페이지 수정

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,8 @@
-import CardCountStatus from "@/components/title/CardCountStatus";
 
 export default function MarketPlace() {
   return (
     <>
       <h1>Market Place</h1>
-      <CardCountStatus page='mySalePhotocard' />
     </>
   );
 }
