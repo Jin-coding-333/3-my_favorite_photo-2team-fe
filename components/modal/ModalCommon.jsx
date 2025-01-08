d0c5d299054fb9477934c9d36f714f820318fab6
taehoone <ghost111789@gmail.comm>
2025-01-08 11:52:31 +0900

    Modal 기능구현 type형태로 변경 진행중

--- components/modal/ModalCommon.jsx
+++ components/modal/ModalCommon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '@/styles/components/modal/Modal.module.css';
 import Button from '@/components/button/Button.jsx';
 
-export default function Modal({ isOpen, onClose, onConfirm, type, cardName, grade, count }) {
+export default function Modal({ isOpen, onClose, onConfirm, type, Data = {} }) {
   if (!isOpen) return null;
 
   const modalTypes = {
@@ -19,27 +19,43 @@ export default function Modal({ isOpen, onClose, onConfirm, type, cardName, grad
     },
     exchangeApproval: {
       title: '교환 제시 승인',
-      message: `[${grade}|${cardName}]카드와의 교환을 승인하시겠습니까?`,
+      message: (
+        <>
+          [{Data.grade} | {Data.cardName}] 카드와의 교환을 승인하시겠습니까?
+        </>
+      ),
       confirmButtonText: '승인하기',
     },
     exchangeRejection: {
       title: '교환 제시 거절',
-      message: `[${grade}|${cardName}]카드와의 교환을 거절하시겠습니까?`,
+      message: (
+        <>
+          [{Data.grade} | {Data.cardName}] 카드와의 교환을 거절하시겠습니까?
+        </>
+      ),
       confirmButtonText: '거절하기',
     },
     buyPhotoCard: {
       title: '포토카드 구매',
-      message: `[${grade}|${cardName}]${count}장을 구매하시겠습니까?`,
+      message: (
+        <>
+          [{Data.grade} | {Data.cardName}] {Data.count}장을 구매하시겠습니까?
+        </>
+      ),
       confirmButtonText: '구매하기',
     },
     stopSelling: {
       title: '포토카드 판매 내리기',
-      message: '정말로 판매를 중단하기겠습니까?',
+      message: '정말로 판매를 중단하시겠습니까?',
       confirmButtonText: '판매 내리기',
     },
   };
 
-  const { title, message, confirmButtonText } = modalTypes[type] || modalTypes['warning'];
+  const { title, message, confirmButtonText } = modalTypes[type] || {
+    title: '타이틀메세지',
+    message: '메세지가 나타납니다',
+    confirmButtonText: '확인버튼',
+  };
 
   return (
     <div className={styles.overlay}>
