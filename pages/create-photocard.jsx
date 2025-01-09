042720addc58ae63152ad112f1bc4bf4672196ae
Jinhan <wlsgks333@naver.com>
2025-01-09 09:28:11 +0900

    chore: 파일 이름 변경

--- pages/create-photocard.jsx
+++ pages/create-photocard.jsx
@@ -1,6 +1,6 @@
 import InputDropdownGenre from '@/components/inputDropdown/InputDropdownGenre';
 import InputDropdownGrade from '@/components/inputDropdown/InputDropdownGrade';
-import styles from '@/styles/pages/create-photocard.module.css';
+import styles from '@/styles/pages/CreatePhotocard.module.css';
 import { useState } from 'react';
 
 export default function CreatePhotocard() {
