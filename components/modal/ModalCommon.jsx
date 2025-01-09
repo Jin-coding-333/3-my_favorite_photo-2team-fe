0eeaa1541e6e1aee0b2da366e65fc121c6fcd528
taehoone <ghost111789@gmail.comm>
2025-01-09 09:28:42 +0900

    Modal 구현 type형태 변경

--- components/modal/ModalCommon.jsx
+++ components/modal/ModalCommon.jsx
@@ -19,29 +19,17 @@ export default function Modal({ isOpen, onClose, onConfirm, type, Data = {} }) {
     },
     exchangeApproval: {
       title: '교환 제시 승인',
-      message: (
-        <>
-          [{Data.grade} | {Data.cardName}] 카드와의 교환을 승인하시겠습니까?
-        </>
-      ),
+      message: `[${Data.grade} | ${Data.cardName}] 카드와의 교환을 승인하시겠습니까?`,
       confirmButtonText: '승인하기',
     },
     exchangeRejection: {
       title: '교환 제시 거절',
-      message: (
-        <>
-          [{Data.grade} | {Data.cardName}] 카드와의 교환을 거절하시겠습니까?
-        </>
-      ),
+      message: `[${Data.grade} | ${Data.cardName}] 카드와의 교환을 거절하시겠습니까?`,
       confirmButtonText: '거절하기',
     },
     buyPhotoCard: {
       title: '포토카드 구매',
-      message: (
-        <>
-          [{Data.grade} | {Data.cardName}] {Data.count}장을 구매하시겠습니까?
-        </>
-      ),
+      message: `[${Data.grade} | ${Data.cardName}] ${Data.count}장을 구매하시겠습니까?`,
       confirmButtonText: '구매하기',
     },
     stopSelling: {
@@ -52,9 +40,9 @@ export default function Modal({ isOpen, onClose, onConfirm, type, Data = {} }) {
   };
 
   const { title, message, confirmButtonText } = modalTypes[type] || {
-    title: '타이틀메세지',
-    message: '메세지가 나타납니다',
-    confirmButtonText: '확인버튼',
+    title: '',
+    message: '',
+    confirmButtonText: '',
   };
 
   return (
@@ -65,10 +53,65 @@ export default function Modal({ isOpen, onClose, onConfirm, type, Data = {} }) {
         </button>
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.message}>{message}</p>
-        <Button type="primary" size="m" onClick={onConfirm} className={styles.confirmButton}>
+        <Button type="primary" onClick={onConfirm} className={styles.confirmButton}>
           {confirmButtonText}
         </Button>
       </div>
     </div>
   );
 }
+
+// import React, { useState } from 'react';
+// import Modal from '@/components/modal/ModalCommon.jsx';
+
+// export default function App() {
+//   const [isModalOpen, setModalOpen] = useState(false);
+//   const [modalType, setModalType] = useState('login');
+//   const [Data, setData] = useState({});
+
+//   const handleOpenModal = (type) => {
+//     setModalType(type);
+//     setModalOpen(true);
+//   };
+
+//   const handleCloseModal = () => {
+//     setModalOpen(false);
+//   };
+
+//   const handleConfirm = () => {
+//     alert(`확인 버튼 클릭`); //page적용시 삭제 예정
+//     setModalOpen(false); //현재는 false로 모달 닫기로만 구현, page적용시 클릭했을때 닫기와 동시에 다음 로직 구현필요
+//   };
+
+//   return (
+//     <div>
+//       {/* //렌더링 테스트를 위해 적용 page적용시 삭제예정 */}
+//       <button onClick={() => handleOpenModal('login')} style={{ color: 'orange' }}>
+//         로그인 모달 열기
+//       </button>{' '}
+//       <br />
+//       <button onClick={() => handleOpenModal('exchangeApproval')} style={{ color: 'orange' }}>
+//         교환 제시 승인 모달 열기
+//       </button>
+//       <br />
+//       <button onClick={() => handleOpenModal('exchangeRejection')} style={{ color: 'orange' }}>
+//         교환 제시 거절
+//       </button>
+//       <br />
+//       <button onClick={() => handleOpenModal('buyPhotoCard')} style={{ color: 'orange' }}>
+//         포토 카드 구매
+//       </button>
+//       <br />
+//       <button onClick={() => handleOpenModal('stopSelling')} style={{ color: 'orange' }}>
+//         포토 카드 판매 내리기
+//       </button>
+//       {/* //메인 기능 모달창 구현 */}
+//       <Modal
+//         isOpen={isModalOpen}
+//         onClose={handleCloseModal}
+//         onConfirm={handleConfirm}
+//         type={modalType}
+//       />
+//     </div>
+//   );
+// }
--- pages/result.jsx
+++ pages/result.jsx
@@ -1,63 +1,7 @@
-// export default function Result() {
-//   return (
-//     <>
-//       <h1>Result</h1>
-//     </>
-//   );
-// }
-
-import React, { useState } from 'react';
-import Modal from '@/components/modal/ModalCommon.jsx';
-
-export default function App() {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [modalType, setModalType] = useState('login');
-  const [Data, setData] = useState({});
-
-  const handleOpenModal = (type) => {
-    setModalType(type);
-    setModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalOpen(false);
-  };
-
-  const handleConfirm = () => {
-    alert(`확인 버튼 클릭`); //page적용시 삭제 예정
-    setModalOpen(false); //현재는 false로 모달 닫기로만 구현, page적용시 클릭했을때 닫기와 동시에 다음 로직 구현필요
-  };
-
+export default function Result() {
   return (
-    <div>
-      {/* //렌더링 테스트를 위해 적용 page적용시 삭제예정 */}
-      <button onClick={() => handleOpenModal('login')} style={{ color: 'orange' }}>
-        로그인 모달 열기
-      </button>{' '}
-      <br />
-      <button onClick={() => handleOpenModal('exchangeApproval')} style={{ color: 'orange' }}>
-        교환 제시 승인 모달 열기
-      </button>
-      <br />
-      <button onClick={() => handleOpenModal('exchangeRejection')} style={{ color: 'orange' }}>
-        교환 제시 거절
-      </button>
-      <br />
-      <button onClick={() => handleOpenModal('buyPhotoCard')} style={{ color: 'orange' }}>
-        포토 카드 구매
-      </button>
-      <br />
-      <button onClick={() => handleOpenModal('stopSelling')} style={{ color: 'orange' }}>
-        포토 카드 판매 내리기
-      </button>
-      {/* //메인 기능 모달창 구현 */}
-      <Modal
-        isOpen={isModalOpen}
-        onClose={handleCloseModal}
-        onConfirm={handleConfirm}
-        type="login"
-        Data={Data}
-      />
-    </div>
+    <>
+      <h1>Result</h1>
+    </>
   );
 }
