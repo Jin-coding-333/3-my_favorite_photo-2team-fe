c8ba77b731d845991a57cf21e0c93f583bf7ddf4
heziss <heziss0965@gmail.com>
2025-01-10 09:45:08 +0900

    fix: margin 값 수정

--- layout/PageLayout.jsx
+++ layout/PageLayout.jsx
@@ -1,12 +1,11 @@
-export default function TitleLayout({children}) {
+export default function PageLayout({children}) {
   return (
     <div 
     style={{
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center',
-        margin: '60px 220px 0',
+        margin: '60px 20px 0',
       }}
     >
       {children}
