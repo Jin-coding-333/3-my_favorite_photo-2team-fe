d4ab93f14b3db8db0c3cbb7064d7968806fa73d2
taehoone <ghost111789@gmail.comm>
2025-01-10 14:46:49 +0900

    구매자,판매자자page구현 (진행중)

--- pages/Buyer.jsx
+++ pages/Buyer.jsx
@@ -1,12 +1,22 @@
 import Title from '@/components/title/Title.jsx';
-import styles from '@/styles/components/Buyer/buyer.module.css';
+import styles from '@/styles/components/Buyer/Buyer.module.css';
 import Card from '@/components/card/Card.jsx';
 export default function Buyer() {
   return (
-    <>
-      <h1 className={styles.title1}>마켓플레이스</h1>
-      <Title title="우리집 앞마당" size="L" />
-      <Card type="buy" grade="RARE" genre="Fantasy" userNickName="John Doe"></Card>
-    </>
+    <div>
+      <h1 className={styles.title}>마켓플레이스</h1>
+      <Title className={styles.cardDetail} title="우리집 앞마당" size="L" />
+      <div className={styles.centerContent}>
+        <img className={styles.cardImg} src="#" />
+        <Card
+          className={styles.Card}
+          type="buy"
+          grade="RARE"
+          genre="Fantasy"
+          userNickName="John Doe"
+        ></Card>
+      </div>
+      <Title title="교환 제시 목록" size="L" />
+    </div>
   );
 }
