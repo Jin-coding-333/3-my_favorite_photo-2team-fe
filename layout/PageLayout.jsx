bc4af3490eb5b1d9c5eca8f9f2e58653059f2383
heziss <heziss0965@gmail.com>
2025-01-13 18:57:19 +0900

    fix: 페이지 레이아웃 미디어쿼리 수정

--- layout/PageLayout.jsx
+++ layout/PageLayout.jsx
@@ -1,12 +1,10 @@
-import styles from "@/styles/layout/PageLayout.module.css"
+import styles from "@/styles/layout/PageLayout.module.css";
 
-export default function PageLayout({children}) {
+export default function PageLayout({ children, fixedButtonLabel, onButtonClick }) {
   return (
     <div className={styles.pageLayout}>
       <div className={styles.titleLayout}></div>
-        <div className={styles.contentLayout}>
-          {children}
-        </div>
+      <div className={styles.contentLayout}>{children}</div>
     </div>
-  )
+  );
 }
