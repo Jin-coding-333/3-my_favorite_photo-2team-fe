f7373c69534315be40f7f65737759c51d31fca6b
taehoone <ghost111789@gmail.comm>
2025-01-13 10:13:55 +0900

    구매자 판매자 기능구현 작업중

--- pages/Buyer.jsx
+++ pages/Buyer.jsx
@@ -4,19 +4,21 @@ import Card from '@/components/card/Card.jsx';
 export default function Buyer() {
   return (
     <div>
-      <h1 className={styles.title}>마켓플레이스</h1>
-      <Title className={styles.cardDetail} title="우리집 앞마당" size="L" />
-      <div className={styles.centerContent}>
-        <img className={styles.cardImg} src="#" />
-        <Card
-          className={styles.Card}
-          type="buy"
-          grade="RARE"
-          genre="Fantasy"
-          userNickName="John Doe"
-        ></Card>
+      <div className={styles.Container}>
+        <h1 className={styles.title}>마켓플레이스</h1>
+        <Title className={styles.cardDetail} title="우리집 앞마당" size="L" />
+        <div className={styles.centerContent}>
+          <img className={styles.cardImg} src="#" />
+          <Card
+            className={styles.Card}
+            type="buy"
+            grade="RARE"
+            genre="Fantasy"
+            userNickName="John Doe"
+          ></Card>
+        </div>
+        <Title title="교환 제시 목록" size="L" />
       </div>
-      <Title title="교환 제시 목록" size="L" />
     </div>
   );
 }
