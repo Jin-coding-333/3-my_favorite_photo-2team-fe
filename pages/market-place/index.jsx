7d3e82819e86aac98326e33b357d014661dbe879
heziss <heziss0965@gmail.com>
2025-01-14 15:38:28 +0900

    feat: 마켓플레이스 타이틀, 페이지 레이아웃, 미디어쿼리 744px일 때 화면 아래에 버튼 고정

--- pages/market-place/index.jsx
+++ pages/market-place/index.jsx
@@ -1,8 +1,31 @@
+import Button from "@/components/button/Button";
+import Title from "@/components/title/Title";
+import PageLayout from "@/layout/PageLayout";
+import styles from "@/styles/pages/MarketPlace.module.css"
 
 export default function MarketPlace() {
   return (
     <>
-      <h1>Market Place</h1>
+      <PageLayout>
+        <Title
+          title="마켓플레이스"
+          size="L"
+          buttonText="나의 포토카드 판매하기"
+          buttonType="primary"
+          onButtonClick={() => alert('판매하기')}
+          variant="secondaryTitle"
+          />
+      </PageLayout>
+      <div className={styles.fixedButton} >
+        <Button
+          size="m"
+          type="primary"
+          onClick={() => alert('판매하기')}
+        >
+        나의 포토카드 판매하기  
+        </Button>      
+        </div>
+
     </>
   );
 }
