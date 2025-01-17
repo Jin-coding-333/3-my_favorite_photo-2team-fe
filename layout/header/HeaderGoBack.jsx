6a93c091fc87e56e1521a649c99105f722d14f7a
Jinhan <wlsgks333@naver.com>
2025-01-17 14:10:36 +0900

    feat: Header 모바일 버전 뒤로 가기 버튼 적용

--- layout/header/HeaderGoBack.jsx
+++ layout/header/HeaderGoBack.jsx
@@ -1,19 +1,42 @@
-import styles from '@/styles/layout/header/HeaderGoBack.module.css';
+import styles from '@/styles/layout/Header.module.css';
 import Image from 'next/image';
-import goBackImg from '/icon/type=back.png';
 
-export default function HeaderGoBack() {
+export default function HeaderGoBack({ currentUrl }) {
+  let title = '';
+
+  if (/^\/market-place\/photocard\/\d+$/.test(currentUrl)) {
+    title = '마켓플레이스';
+  }
+
+  if (currentUrl === '/my-gallery') {
+    title = '마이갤러리';
+  }
+
+  if (/^\/my-gallery\/my-photocard\/\d+$/.test(currentUrl)) {
+    title = '';
+  }
+
+  if (currentUrl === '/my-sale-photocard') {
+    title = '나의 판매 포토카드';
+  }
+
   return (
     <div className={styles.headerGoBackContainer}>
-      <div className={styles.goBackImg}>
+      <div
+        className={styles.goBackImg}
+        style={{
+          position: 'relative',
+        }}
+      >
         <Image
-          src={goBackImg}
+          src='/icon/type=back.png'
           alt="Go Back Image"
-          layout="fill"
-          objectFit="cover"
+          fill
+          style={{ objectFit: 'cover' }}
         />
       </div>
-      <h1 className={styles.goBackText}>{'ㅁㄴㅇㅇ'}</h1>
+      <h1 className={styles.goBackText}>{title}</h1>
+      <div style={{ width: '20px' }}></div>
     </div>
   );
 }
