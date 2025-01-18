eea913b42801f96b5b07b580ced4f9486064f272
Jinhan <wlsgks333@naver.com>
2025-01-18 22:55:40 +0900

    chore: slick install

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,11 @@ import '@/styles/globals.css';
 import Head from 'next/head';
 import Header from '@/layout/header/Header';
 import Provider from '@/contexts/Provider';
+// pages/_app.js
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import '@/styles/globals.css'; // 기존 스타일 파일
+
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
