5ad47b9d9f8bee392dc81cb3af816cfe20d69cba
jonghun4 <xwhdgns@gmail.com>
2025-01-20 09:41:10 +0900

    PhotoCardModal 작성

---
+++ components/modal/marketPlaceModal/PhotoCardModal.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+import Image from 'next/image';
+import BackImg from '@/public/icon/type=back.png';
+import slashImg from '@/public/icon/type=-.png';
+import CloseBtnImg from '@/public/icon/type=close.png';
+import testImg from '@/public/img/image1.png';
+import Title from '@/components/title/Title';
+import CardDetail from '@/components/card/CardDetail';
+import InputDropdownGrade from '@/components/dropdown/inputDropdown/InputDropdownGrade';
+import InputDropdownGenre from '@/components/dropdown/inputDropdown/InputDropdownGenre';
+import InputTextBox from '@/components/input/InputTextBox';
+import Button from '@/components/button/Button';
+import styles from '@/styles/components/modal/marketPlaceModal/PhotoCardModal.module.css';
+import PhotoModal from '../photoModal/PhotoModal';
+
+const Card = {
+  name: '테스트용임',
+};
+
+export default function PhotoCardModal({ isOpen, onClose, isEdit }) {
+  const [form, setForm] = useState({ genre: '', grade: '' });
+
+  if (!isOpen) return null;
+
+  return (
+    <PhotoModal isModal={isOpen} handleModal={onClose} modalType="lastPage">
+      <div className={styles.modalLayer}>
+        <div className={styles.modalCloseBtns}>
+          <div className={styles.modalName}>{isEdit ? '수정하기' : '나의 포토카드 판매하기'}</div>
+        </div>
+        <div className={styles.scrollableContainer}>
+          <Title title={Card.name} className={styles.headerTitle} variant="default" />
+          <div className={styles.itemCard}>
+            <Image src={testImg} alt="테스트 이미지" className={styles.imageSize} />
+            <CardDetail userNickName="테스트" />
+          </div>
+          <div className={styles.formLayer}>
+            <Title title="교환 희망 정보" className={styles.formTitle} variant="default" />
+            <form className={styles.forms}>
+              <InputDropdownGenre
+                setForm={setForm}
+                className={styles.dropdownLayerSize}
+                classNameSize={styles.dropdownSize}
+              />
+              <InputDropdownGrade
+                setForm={setForm}
+                className={styles.dropdownLayerSize}
+                classNameSize={styles.dropdownSize}
+              />
+            </form>
+            <InputTextBox type="exchangeRequest" className={styles.textBox} />
+          </div>
+          <div className={styles.Buttons}>
+            <Button type="secondary" className={styles.modalButton}>
+              취소하기
+            </Button>
+            <Button type="primary" className={styles.modalButton}>
+              {isEdit ? '수정하기' : '판매하기'}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </PhotoModal>
+  );
+}
+
+/*
+import PhotoCardModal from '@/components/modal/marketPlaceModal/PhotoCardModal';
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalType, setModalType] = useState('');
+
+  const openSellModal = () => {
+    setModalType('sell');
+    setIsModalOpen(true);
+  };
+
+  const openEditModal = () => {
+    setModalType('edit');
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+<button onClick={openSellModal} style={{ background: '#ffffff' }}>
+  판매하기
+</button>
+<br />
+<button onClick={openEditModal} style={{ background: '#ffffff' }}>
+  수정하기
+</button>
+<PhotoCardModal isOpen={isModalOpen} onClose={closeModal} isEdit={modalType === 'edit'} />
+*/
