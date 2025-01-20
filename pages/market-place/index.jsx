990f22d47a6f13a5867966ca7f91e33abcdd0a8d
jonghun4 <xwhdgns@gmail.com>
2025-01-20 10:31:47 +0900

    포토카드 모달 css 2차 수정

--- pages/market-place/index.jsx
+++ pages/market-place/index.jsx
@@ -4,11 +4,34 @@ import PageLayout from '@/layout/PageLayout';
 import styles from '@/styles/pages/MarketPlace.module.css';
 import MarketPlaceSearch from '@/components/pages/SearchSortComponent/MarketPlaceSearch';
 import PhotocardList from '@/components/pages/market-place/PhotocardList';
+import { useState } from 'react';
+import PhotoCardModal from '@/components/modal/marketPlaceModal/PhotoCardModal';
 
 export default function MarketPlace() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalType, setModalType] = useState('');
+
+  const openSellModal = () => {
+    setModalType('sell');
+    setIsModalOpen(true);
+  };
+
+  const openEditModal = () => {
+    setModalType('edit');
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       <PageLayout>
+        <button onClick={openSellModal} style={{ background: '#ffffff' }}>
+          판매하기
+        </button>
+        <PhotoCardModal isOpen={isModalOpen} onClose={closeModal} isEdit={modalType === 'edit'} />
         <div className={styles.marketPlaceTitle}>
           <Title
             title="마켓플레이스"
