d5470099ee5dc34043ef8246ff4b0682c066b2c7
Jinhan <wlsgks333@naver.com>
2025-05-16 17:23:08 +0900

    🐛 fix : build error로 인한 코드 수정

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,15 @@ const nextConfig = {
     ignoreDuringBuilds: true, // 빌드 중 ESLint 검사 무시
   },
   images: {
-    domains: ['th.bing.com'],
     remotePatterns: [
       {
-        protocol: 'http',
-        // hostname: 'localhost',
-        hostname: 'https://my-favorite-photo.onrender.com',
-        port: '8000', // 포트 번호 지정
-        pathname: '/upload/**', // 업로드 경로 패턴
+        protocol: 'https',
+        hostname: 'th.bing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'my-favorite-photo.onrender.com',
+        pathname: '/upload/**',
       },
     ],
   },
