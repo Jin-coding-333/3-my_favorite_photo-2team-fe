976886ea731e64b480d223140133735f3dbbd7b8
Jinhan <wlsgks333@naver.com>
2025-05-21 00:02:04 +0900

    fix : 포토카드 렌더링 되도록 설정
    백엔드 서버 uploads 폴더가 `.gitignore`에 들어가 있어서 배포된 서버에서 인식을 못하고 있던 에러 사항을 해결함

--- components/pages/my_gallery/PhotocardList.jsx
+++ components/pages/my_gallery/PhotocardList.jsx
@@ -2,25 +2,34 @@ import PhotoCard from "@/components/card/photoCard/PhotoCard";
 import styles from "@/styles/components/pages/market-place/PhotocardList.module.css";
 import Link from "next/link";
 import usePhotocardList from '@/lib/hooks/usePhotocardList';
-import photocards from '@/lib/data/cardsData';
 
 export default function PhotocardList() {
   const photocardList = usePhotocardList();
 
   // 데이터가 없을 때의 처리
-  if (!photocards || photocards.length === 0) {
+  if (!photocardList || photocardList.length === 0) {
     return <div className={styles.photocardListContainer}>포토카드가 없습니다.</div>;
   }
 
   return (
     <div className={styles.photocardListContainer}>
-      {photocards.map((photocard) => (
+      {photocardList.map((photocard) => (
         <div key={photocard.id} className={styles.photocard}>
           <Link href={`/my-gallery/my-photocard/${photocard.id}`}>
             <PhotoCard
               cardType='original'
-              isSoldOut={photocard.isSoldOut}
-              data={photocard}
+              data={{
+                card: {
+                  imagePath: photocard.imagePath,
+                  grade: photocard.grade,
+                  genre: photocard.genre
+                },
+                name: photocard.name,
+                price: photocard.price,
+                totalQuantity: 1,
+                remainingQuantity: 1,
+                user: photocard.user
+              }}
             />
           </Link>
         </div>
